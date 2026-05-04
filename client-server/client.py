d271b824de6e6c9dca99c393c068c80fa49ba995
Lauane Mateus <lauanemo16@gmail.com>
2026-05-04 14:50:19 -0300

    separando cliente do servidor para conectar em máquinas diferentes na AWS

---
+++ client-server/client.py
@@ -0,0 +1,9 @@
+def client():
+  context = zmq.Context()
+  socket  = context.socket(zmq.REQ)       # create request socket
+
+  socket.connect("tcp://52.7.58.214:5679") # block until connected
+  socket.send(b"Hello world")             # send message
+  message = socket.recv()                 # block until response
+  socket.send(b"STOP")                    # tell server to stop
+  print(message.decode())                 # print result
---
+++ client-server/server.py
@@ -0,0 +1,14 @@
+import zmq
+
+def server():
+  context = zmq.Context()
+  socket  = context.socket(zmq.REP)       # create reply socket
+  socket.bind("tcp://*:5679")            # bind socket to address
+
+  while True:
+    message = socket.recv()               # wait for incoming message
+    if not "STOP" in str(message):        # if not to stop...
+      reply = str(message.decode())+'*'   # append "*" to message
+      socket.send(reply.encode())         # send it away (encoded)
+    else:                         
+      break                               # break out of loop and end
